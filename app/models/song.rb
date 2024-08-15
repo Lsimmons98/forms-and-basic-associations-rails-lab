4cfc82c44c9c68441d309447d8a757b4fc551a51
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-15 15:33:01 -0400

    song form

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,30 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def build_artist(name:)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.name if artist
+  end
+
+  def artist_name=(new_name)
+    build_artist(name: new_name)
+  end
+
+  def note_contents
+    notes.map do |note|
+     note.content
+    end
+  end
+
+  def note_contents=(content_strings)
+    content_strings.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note
+    end
+  end
 end
